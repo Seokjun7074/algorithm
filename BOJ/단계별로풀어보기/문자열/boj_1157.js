fa274aa273ff93e3af031337a82e94a232295ab4
seokjun <sukjune96@gmail.com>
2022-04-07 03:06:15 +0900

    boj_1157

---
+++ BOJ/단계별로풀어보기/문자열/boj_1157.js
@@ -0,0 +1,31 @@
+const { kStringMaxLength } = require("buffer");
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filepath).toString();
+
+input = input.toUpperCase();
+let arr = [];
+let maxString = "";
+for (let i = 65; i <= 90; i++) {
+  arr.push(String.fromCharCode(i));
+}
+
+let idx = new Array(arr.length).fill(0);
+
+for (let i = 0; i < input.length; i++) {
+  const index = arr.indexOf(input[i]);
+  idx[index]++;
+}
+
+let max = Math.max(...idx);
+let maxIdx = idx.indexOf(max);
+
+for (let i = 0; i < idx.length; i++) {
+  if (max === idx[i] && maxIdx != i) {
+    maxString = "?";
+    break;
+  } else {
+    maxString = arr[maxIdx];
+  }
+}
+console.log(maxString);
