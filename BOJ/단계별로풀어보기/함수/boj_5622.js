068ee76ade7f832eb803973faef288baf00cdb89
seokjun <sukjune96@gmail.com>
2022-04-20 13:52:56 +0900

    boj_5622(수정)

--- BOJ/단계별로풀어보기/함수/boj_5622.js
+++ BOJ/단계별로풀어보기/함수/boj_5622.js
@@ -2,38 +2,38 @@ const fs = require("fs");
 const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filepath).toString().trim();
 
-// const dial = ["ABC", "DEF", "GHI", "JKL", "MNO", "PORS", "TUV", "WXYZ"];
-// let time = 0;
+const dial = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"];
+let time = 0;
 
-// for (let i = 0; i < input.length; i++) {
-//   for (let j = 0; j < dial.length; j++) {
-//     if (dial[j].includes(input[i])) {
-//       let number = j + 3;
-//       time += number;
-//     }
-//   }
-// }
-// console.log(time);
-
-let answer = 0;
-for (let i = 0; i < input.length; ++i) {
-  let c = input[i];
-  if (c >= "W") {
-    answer += 10;
-  } else if (c >= "T") {
-    answer += 9;
-  } else if (c >= "P") {
-    answer += 8;
-  } else if (c >= "M") {
-    answer += 7;
-  } else if (c >= "J") {
-    answer += 6;
-  } else if (c >= "G") {
-    answer += 5;
-  } else if (c >= "D") {
-    answer += 4;
-  } else if (c >= "A") {
-    answer += 3;
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < dial.length; j++) {
+    if (dial[j].includes(input[i])) {
+      let number = j + 3;
+      time += number;
+    }
   }
 }
-console.log(answer);
+console.log(time);
+
+// let answer = 0;
+// for (let i = 0; i < input.length; ++i) {
+//   let c = input[i];
+//   if (c >= "W") {
+//     answer += 10;
+//   } else if (c >= "T") {
+//     answer += 9;
+//   } else if (c >= "P") {
+//     answer += 8;
+//   } else if (c >= "M") {
+//     answer += 7;
+//   } else if (c >= "J") {
+//     answer += 6;
+//   } else if (c >= "G") {
+//     answer += 5;
+//   } else if (c >= "D") {
+//     answer += 4;
+//   } else if (c >= "A") {
+//     answer += 3;
+//   }
+// }
+// console.log(answer);
