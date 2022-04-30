14eec9688e21aa52f327a2336b5380e542c21ad1
seokjun <sukjune96@gmail.com>
2022-05-01 00:41:30 +0900

    boj_1193

--- BOJ/단계별로풀어보기/기본수학1/boj_1193.js
+++ BOJ/단계별로풀어보기/기본수학1/boj_1193.js
@@ -2,15 +2,15 @@ const fs = require("fs");
 const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = parseInt(fs.readFileSync(filepath).toString());
 
-console.log(input);
-
 let group = 1;
 let sum = 1;
 
-while (k <= 5) {
-  k++;
-  sum += k;
-
-  console.log(`k:${k}`);
-  console.log(sum);
+while (input > sum) {
+  group++;
+  sum += group;
+}
+if (group % 2 === 0) {
+  console.log(`${group - sum + input}/${1 + sum - input}`);
+} else {
+  console.log(`${1 + sum - input}/${group - sum + input}`);
 }
