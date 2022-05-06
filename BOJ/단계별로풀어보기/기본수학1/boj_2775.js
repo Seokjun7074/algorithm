402ea28728b71212b55dc5293689c38e5a5ef40a
seokjun <sukjune96@gmail.com>
2022-05-06 19:41:00 +0900

    boj_2775_2

--- BOJ/단계별로풀어보기/기본수학1/boj_2775.js
+++ BOJ/단계별로풀어보기/기본수학1/boj_2775.js
@@ -7,22 +7,46 @@ let input = fs
   .split("\n")
   .map((e) => Number(e));
 
-const testCase = input.shift();
+// const testCase = input.shift();
 
-for (let i = 0; i < testCase; i++) {
-  const k = input.shift();
-  const n = input.shift();
-  const apartment = [];
+// for (let i = 0; i < testCase; i++) {
+//   const k = input.shift();
+//   const n = input.shift();
+//   const apartment = [];
 
-  for (let i = 0; i <= k; i++) {
-    apartment.push([1]);
-    for (let j = 1; j < n; j++) {
-      if (i === 0) {
-        apartment[i].push(j + 1);
-      } else {
-        apartment[i].push(apartment[i][j - 1] + apartment[i - 1][j]);
-      }
+//   for (let i = 0; i <= k; i++) {
+//     apartment.push([1]);
+//     console.log(apartment);
+//     for (let j = 1; j < n; j++) {
+//       if (i === 0) {
+//         apartment[i].push(j + 1);
+//       } else {
+//         apartment[i].push(apartment[i][j - 1] + apartment[i - 1][j]);
+//       }
+//     }
+//   }
+//   console.log(apartment[k][n - 1]);
+// }
+
+const T = input.shift();
+
+for (let i = 0; i < T; i++) {
+  const k = +input.shift();
+  const n = +input.shift();
+
+  // 각 방당 인원수를 저장할 2차원 배열 선언
+  const house = Array.from(Array(k + 1), () => Array(n + 1).fill(0));
+  // 0층 (0층의 i호에는 i명이 산다.)
+  for (let i = 1; i <= n; i++) {
+    house[0][i] = i;
+  }
+  // 1 ~ k층 (k층 n호에는 k-1층 n호 + k층 n-1호만큼 산다.)
+  for (let i = 1; i <= k; i++) {
+    for (let j = 1; j <= n; j++) {
+      house[i][j] = house[i - 1][j] + house[i][j - 1];
     }
   }
-  console.log(apartment[k][n - 1]);
+
+  // k층 n호의 인원수 출력
+  console.log(house[k][n]);
 }
