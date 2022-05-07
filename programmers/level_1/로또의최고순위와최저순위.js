18d3724e5cf8dc4f19c99728d073426c11b7777a
seokjun <sukjune96@gmail.com>
2022-05-08 00:17:15 +0900

    로또의최고순위와최저순위

---
+++ programmers/level_1/로또의최고순위와최저순위.js
@@ -0,0 +1,16 @@
+const lottos = [31, 10, 45, 1, 6, 19];
+const win_nums = [31, 10, 45, 1, 6, 19];
+let count = 0;
+let zero = 0;
+lottos.forEach((e) => {
+  if (win_nums.includes(e)) {
+    count++;
+  }
+  if (e === 0) {
+    zero++;
+  }
+});
+
+const min_money = 7 - count > 6 ? 6 : 7 - count;
+const max_money = min_money - zero < 1 ? 1 : min_money - zero;
+console.log([max_money, min_money]);
