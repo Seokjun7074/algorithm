3a9b0622c3b64e8b4e0b8f381a34d190f08159a5
seokjun <sukjune96@gmail.com>
2022-05-22 09:24:25 +0900

    boj_10757_re

--- BOJ/단계별로풀어보기/기본수학1/boj_10757.js
+++ BOJ/단계별로풀어보기/기본수학1/boj_10757.js
@@ -8,5 +8,20 @@ const B = BigInt(input[1]);
 console.log((A + B).toString());
 
 //BigIntBigInt는
+
 //Number 원시 값이 안정적으로 나타낼 수 있는 최대치인 2^53 - 1보다
 //큰 정수를 표현할 수 있는 내장 객체
+
+// const theBiggestInt = 9007199254740991n;
+
+// const alsoHuge = BigInt(9007199254740991);
+// ↪ 9007199254740991n
+
+// const hugeString = BigInt("9007199254740991");
+// ↪ 9007199254740991n
+
+// const hugeHex = BigInt("0x1fffffffffffff");
+// ↪ 9007199254740991n
+
+// const hugeBin = BigInt("0b11111111111111111111111111111111111111111111111111111");
+// ↪ 9007199254740991n
