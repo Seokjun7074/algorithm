e38c8990225bf173a7d71bec580f685bf91aa9ba
seokjun <sukjune96@gmail.com>
2022-12-10 21:46:39 +0900

    boj_11650

---
+++ BOJ/단계별로풀어보기/정렬/boj_11650.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+
+N = int(input())
+arr = []
+for i in range(N):
+    a, b = map(int, input().split())
+    arr.append([a, b])
+arr.sort()
+for i in range(N):
+    print(arr[i][0], arr[i][1])
