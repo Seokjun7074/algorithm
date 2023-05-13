5faeaf8398d6f189bf2eaedfce677e1ed71dafaa
이석준 <sukjune96@gmail.com>
2023-05-13 15:13:40 +0900

    달리기경주

---
+++ programmers/level_1/달리기경주.py
@@ -0,0 +1,10 @@
+def solution(players, callings):
+    hashmap = dict()
+    for i in range(len(players)):
+        hashmap[players[i]] = i
+    for called in callings:
+        idx = hashmap[called]
+        hashmap[called] -= 1
+        hashmap[players[idx-1]] += 1
+        players[idx-1], players[idx] = players[idx], players[idx-1]
+    return players
