72ede45e4479cbf679e59d0f786bf85fc51c4269
이석준 <sukjune96@gmail.com>
2023-06-05 19:17:01 +0900

    짝지어제거하기

---
+++ programmers/level2/짝지어제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = -1
+    stack = []
+    for x in s:
+        if len(stack) == 0:
+            stack.append(x)
+        elif stack[-1] == x:
+            stack.pop()
+        else:
+            stack.append(x)
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
