aa6f680149a547e7c145ed4b910cd76b51d4588b
seokjun <sukjune96@gmail.com>
2023-06-21 21:46:31 +0900

    boj_7562_나이트의이동

---
+++ BOJ/문제풀이/boj_7562_나이트의읻동.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def bfs(cur_y, cur_x):
+    graph[cur_y][cur_x] = 1
+    q = deque()
+    q.append((cur_y, cur_x))
+    while q:
+        y, x = q.popleft()
+        if y == des_y and x == des_x:
+            return graph[des_y][des_x] - 1
+        dxdy = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
+        for dy, dx in dxdy:
+            ny = y + dy
+            nx = x + dx
+            if -1 < nx < l and -1 < ny < l:
+                if graph[ny][nx] == 0:
+                    graph[ny][nx] = graph[y][x] + 1
+                    q.append((ny, nx))
+
+
+T = int(input())
+for _ in range(T):
+    l = int(input())
+    graph = [[0 for _ in range(l)] for _ in range(l)]
+    cur_y, cur_x = map(int, input().split())
+    des_y, des_x = map(int, input().split())
+    print(bfs(cur_y, cur_x))
