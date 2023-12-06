7f95571ff636e3313e7530c9b0511c25fc3674fc
seokjun <sukjune96@gmail.com>
2023-12-06 22:31:15 +0900

    boj_9461_파도반수열

--- BOJ/문제풀이/boj_9461_파도반수열.py
+++ BOJ/문제풀이/boj_9461_파도반수열.py
@@ -1,9 +1,23 @@
-# import sys
+import sys
 
-# input = sys.stdin.readline
+input = sys.stdin.readline
 
 T = int(input())
+P = [0] * 101
+P[0] = 0
+P[1] = 1
+P[2] = 1
+P[3] = 1
+
+
+def pibo(x):
+    if P[x]:
+        return P[x]
+    else:
+        P[x] = pibo(x - 2) + pibo(x - 3)
+        return P[x]
+
 
 for x in range(T):
     N = int(input())
-    print("s", N)
+    print(pibo(N))
