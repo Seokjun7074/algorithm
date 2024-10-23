4a5e54cbad343dc3aa73138367531e89354bc823
seokjun <sukjune96@gmail.com>
2024-10-23 22:30:51 +0900

    N개의최소공배수

---
+++ programmers/level2/N개의최소공배수.py
@@ -0,0 +1,15 @@
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+
+def solution(arr):
+    answer = 1
+    for i in arr:
+        answer = lcm(answer, i)
+    return answer
