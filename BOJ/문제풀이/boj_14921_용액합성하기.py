15d074deaeadc6bbc3397ac3b6f1ff87914fa2fa
seokjun <sukjune96@gmail.com>
2025-03-04 19:09:17 +0900

    boj_14921_용액합성하기(refactor)

--- BOJ/문제풀이/boj_14921_용액합성하기.py
+++ BOJ/문제풀이/boj_14921_용액합성하기.py
@@ -6,19 +6,17 @@ arr = list(map(int, input().split()))
 
 answer = sys.maxsize
 
-for i in range(N - 1):
-    left = i + 1
-    right = N - 1
-    while left <= right:
-        mid = (left + right) // 2
-        s = arr[i] + arr[mid]
-        if s == 0:
-            answer = 0
-            break
-        if s < 0:
-            left = mid + 1
-        if s > 0:
-            right = mid - 1
-        if abs(s) < abs(answer):
-            answer = s
+left = 0
+right = N - 1
+while left < right:
+    s = arr[left] + arr[right]
+    if s == 0:
+        answer = 0
+        break
+    if s < 0:
+        left += 1
+    if s > 0:
+        right -= 1
+    if abs(s) < abs(answer):
+        answer = s
 print(answer)
