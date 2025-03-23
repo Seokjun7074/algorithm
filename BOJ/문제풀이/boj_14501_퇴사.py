b10fb6d4be4d85d39981ef2636906ea76fd9b5a9
seokjun <sukjune96@gmail.com>
2025-03-23 23:54:27 +0900

    boj_14501_퇴사(DP)

--- BOJ/문제풀이/boj_14501_퇴사.py
+++ BOJ/문제풀이/boj_14501_퇴사.py
@@ -9,20 +9,13 @@ for _ in range(n):
     time.append(t)
     pay.append(p)
 
-v = [False] * n
+dp = [0] * (n + 1)  # dp[i]: i번째 날부터 퇴사일까지 얻을 수 있는 최대 수익
 
-answer = 0
+# 뒤에서부터 DP 계산
+for i in range(n - 1, -1, -1):
+    if i + time[i] <= n:  # 상담을 진행할 수 있는 경우
+        dp[i] = max(dp[i + 1], pay[i] + dp[i + time[i]])
+    else:  # 상담을 진행할 수 없는 경우 (넘어감)
+        dp[i] = dp[i + 1]
 
-
-def dfs(day, money):
-    global answer
-    if day >= n:
-        answer = max(answer, money)
-        return
-    if day + time[day] <= n:
-        dfs(day + time[day], money + pay[day])
-    dfs(day + 1, money)
-
-
-dfs(0, 0)
-print(answer)
+print(dp[0])  # 첫날부터 얻을 수 있는 최대 이익
