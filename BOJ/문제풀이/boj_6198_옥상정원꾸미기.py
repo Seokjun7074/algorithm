fc109711c5d33d2de2e2573600972af531f982ea
seokjun <sukjune96@gmail.com>
2025-03-23 13:40:00 +0900

    boj_6198_옥상정원꾸미기

--- BOJ/문제풀이/boj_6198_옥상정원꾸미기.py
+++ BOJ/문제풀이/boj_6198_옥상정원꾸미기.py
@@ -1,19 +1,18 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 N = int(input())
-arr = []
-answer = 0
-for i in range(N):
-    arr.append((int(input()), i))
-building = deque(arr)
-stack = []  # 계산 중인 건물들
+buildings = [int(input()) for _ in range(N)]
 
-while len(building) > 1:
-    target = building.popleft()
-    stack.append(target)
-    while stack and building and stack[-1] <= building[0]:
+stack = []
+answer = 0
+
+for h in buildings:
+    print(h, stack)
+    while stack and stack[-1] <= h:  # 현재 빌딩보다 낮은 빌딩은 필요 없음 (pop)
         stack.pop()
-    answer += len(stack)
+    print(stack)
+    answer += len(stack)  # 스택에 남아 있는 빌딩 수만큼 count
+    stack.append(h)  # 현재 빌딩 push
+
 print(answer)
