168b2abbd5bdd381aba403d7313b8250fa0def2f
seokjun <sukjune96@gmail.com>
2025-04-11 01:40:28 +0900

    고대문명유적탐사.ver2

--- Codetree/고대문명유적탐사.py
+++ Codetree/고대문명유적탐사.py
@@ -1,91 +1,85 @@
-def rotate(arr, si, sj):    # 90도 시계방향 회전
-    narr = [x[:] for x in arr]
+from collections import deque
+import sys
+sys.stdin = open("../input.txt")
+input = sys.stdin.readline
+
+def rotate(si,sj,arr):
+    new_arr = [x[:] for x in arr]
     for i in range(3):
         for j in range(3):
-            narr[si+i][sj+j]=arr[si+3-j-1][sj+i]
-    return narr
+            new_arr[si+i][sj+j] = arr[si+2-j][sj+i]
+    return new_arr
 
-def bfs(arr,v,si,sj,clr):
-    q = []
-    sset = set()
-    cnt = 0
-
-    q.append((si,sj))
-    v[si][sj]=1
-    sset.add((si,sj))
-    cnt+=1
-
+def bfs(si,sj,v,arr,isReset):
+    q = deque([(si,sj)])
+    v[si][sj] = 1
+    visitedSet = set()
+    visitedSet.add((si,sj))
+    cnt = 1
     while q:
-        ci,cj = q.pop(0)
-        # 네방향, 범위내, 미방문, 조건: 같은 값이면
-        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-            ni,nj = ci+di, cj+dj
-            if 0<=ni<5 and 0<=nj<5 and v[ni][nj]==0 and arr[ci][cj]==arr[ni][nj]:
+        ci,cj = q.popleft()
+        for di,dj in ((0,1), (0,-1),(1,0),(-1,0)):
+            ni = ci+di
+            nj = cj+dj
+            if 0<=ni<5 and 0<=nj<5 and v[ni][nj] == 0 and arr[ni][nj]==arr[ci][cj]:
+                v[ni][nj] = 1
                 q.append((ni,nj))
-                v[ni][nj]=1
-                sset.add((ni,nj))
+                visitedSet.add((ni,nj))
                 cnt+=1
-
-    if cnt>=3:      # 유물이면: cnt 리턴 + clr==1이면 0으로 clear
-        if clr==1:  # 0으로 초기화
-            for i,j in sset:
-                arr[i][j]=0
+    if cnt>=3:
+        if isReset:
+            for i,j in visitedSet:
+                arr[i][j] = 0
         return cnt
-    else:           # 3개 미만이면 0리턴
-        return 0
+    return 0
 
-def count_clear(arr, clr):  # clr==1인 경우 3개이상값들을 0으로 clear
-    v = [[0]*5 for _ in range(5)]
-    cnt = 0
+# arr 대상으로 보물찾기
+def findTreasure(arr, isReset):
+    v = [[0] * 5 for _ in range(5)]
+    cnt= 0
     for i in range(5):
-        for j in range(5):  # 미방문인 경우 같은 값이면 fill
-            if v[i][j]==0:
-                # 같은 값이면, 3개 이상인 경우
-                t = bfs(arr,v,i,j,clr)
-                cnt+=t
+        for j in range(5):
+            # 각 좌표를 돌며 같은놈 BFS 탐색
+            cnt+=bfs(i,j,v,arr,isReset)
     return cnt
 
 K, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(5)]
+cell = [list(map(int, input().split())) for _ in range(5)]
 lst = list(map(int, input().split()))
-ans = []
+refill_list = deque(lst)
+result = []
 
+# findTreasure(rotate(1,1,cell))
 for _ in range(K):  # K턴을 진행(유물이 없는 경우 즉시종료)
-    #[1] 탐사진행
-    mx_cnt = 0
-    searchArr = None
-    for rot in range(1, 4):     # 회전수->열->행 (작은순)
-        for sj in range(3):
-            for si in range(3):
-                # rot 회수만큼 90도 시계방향 회전 => narr
-                narr = [x[:] for x in arr]
-                for _ in range(rot):
-                    narr = rotate(narr, si, sj)
-
-                # 유무개수 카운트
-                t = count_clear(narr, 0)
-                if mx_cnt < t:      # 최대개수
-                    mx_cnt = t
-                    searchArr = narr
-
-    # 유물이 없는 경우 턴 즉시종료
-    if mx_cnt==0 or not searchArr:
+    first_price = 0
+    target_arr = None # 첫번째 탐색 이후에 지정한 배열
+    for angle in range(1,4):
+        for j in range(3):
+            for i in range(3):
+                # 회전시키기
+                new_arr = [x[:] for x in cell]
+                for _ in range(angle):
+                    new_arr = rotate(i,j,new_arr)
+                # 회전된 배열 대상으로 보물 찾기
+                t = findTreasure(new_arr, False)
+                if first_price<t:
+                    target_arr = new_arr
+                    first_price = t
+    # 찾은게 없으면 종료
+    if first_price ==0 or not target_arr:
         break
-
-    #[2] 연쇄획득
-    cnt = 0
-    arr = searchArr
+    # 반복 돌리기
+    answer = 0
+    cell = target_arr
     while True:
-        t = count_clear(arr, 1)
-        if t==0:
-            break   # 연쇄획득 종료 => 다음 턴으로..
-        cnt += t    # 획득한 유물 개수 누적
-
-        # arr의 0값인 부분 리스트에서 순서대로 추가
+        t = findTreasure(cell, True)
+        if t == 0:
+            break
+        answer+=t
         for j in range(5):
             for i in range(4,-1,-1):
-                if arr[i][j]==0:
-                    arr[i][j]=lst.pop(0)
-
-    ans.append(cnt) # 이번턴 연쇄획득한 개수 추가
-print(*ans)
+                if cell[i][j]==0:
+                    new_num = refill_list.popleft()
+                    cell[i][j] = new_num
+    result.append(answer)
+print(*result)
