74a94d97da059864b6f8684b1a07ef1f95ad6bbe
seokjun <sukjune96@gmail.com>
2025-04-24 17:47:16 +0900

    boj_2668_숫자고르기.ver2

--- BOJ/문제풀이/boj_2668_숫자고르기.py
+++ BOJ/문제풀이/boj_2668_숫자고르기.py
@@ -1,38 +1,24 @@
 import sys
 
 input = sys.stdin.readline
-N = int(input())
-arr = [[i for i in range(N + 1)]]
-tmp_arr = [0]
-for _ in range(N):
-    tmp_arr.append(int(input()))
-arr.append(tmp_arr)
 
 
-def dfs(start, cur, v):
-    v[cur] += 1
-    nextNum = arr[1][cur]
-    if start == nextNum:
-        v[nextNum] += 1
-        for i in range(1, N + 1):
-            if v[i] == 2:
-                answer[i] = 1
-        return
-    if v[nextNum] == 0:
-        v[nextNum] += 1
-        dfs(start, nextNum, v)
+def dfs(cur, start):
+    visited[cur] = True
+    nextNum = data[cur]
+    if not visited[nextNum]:
+        dfs(nextNum, start)
+    elif visited[nextNum] and nextNum == start:
+        result.append(start)
 
 
-answer = [0] * (N + 1)
-for i in range(1, N + 1):
-    v = [0] * (N + 1)
-    dfs(i, i, v)
-
-cnt = 0
-for num in answer:
-    if num > 0:
-        cnt += 1
-print(cnt)
-for i in range(1, N + 1):
-    if answer[i] > 0:
-        print(i)
+n = int(input())
+data = [0] + [int(input()) for _ in range(n)]
+result = []
+for i in range(1, n + 1):
+    visited = [False] * (n + 1)
+    dfs(i, i)
+print(len(result))
+result.sort()
+for i in result:
+    print(i)
