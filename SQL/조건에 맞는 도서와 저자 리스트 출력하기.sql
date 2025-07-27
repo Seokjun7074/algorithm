4905bce5864782e198e7c5eb96e6bec41afae6df
seokjun <sukjune96@gmail.com>
2025-07-27 16:53:49 +0900

    조건에 맞는 도서와 저자 리스트 출력하기

---
+++ SQL/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,4 @@
+SELECT B.BOOK_ID, A.AUTHOR_NAME, date_format(B.PUBLISHED_DATE, '%Y-%m-%d') as PUBLISHED_DATE
+from BOOK B join AUTHOR A on A.AUTHOR_ID = B.AUTHOR_ID
+where B.CATEGORY = '경제'
+order by PUBLISHED_DATE
