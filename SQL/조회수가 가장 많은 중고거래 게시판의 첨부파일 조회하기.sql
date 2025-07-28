c62bf2a4de87139da9f38d36b5776439c84d1d55
Seokjun7074 <sukjune96@gmail.com>
2025-07-28 17:14:11 +0900

    조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기

---
+++ SQL/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,5 @@
+SELECT concat('/home/grep/src/',F.BOARD_ID,'/',F.FILE_ID,F.FILE_NAME,F.FILE_EXT) as "FILE_PATH"
+from  USED_GOODS_FILE F
+where F.BOARD_ID =
+(select BOARD_ID from USED_GOODS_BOARD order by VIEWS desc limit 1)
+order by F.FILE_ID desc
